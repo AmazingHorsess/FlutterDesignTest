b139148af97283a9938500193ce6eacdb77ea82e
AmazingHorsess <amazinghorsess@gmail.com>
2023-04-05 23:17:17 +0600

    Initial commit

---
+++ lib/Screens/LandingPage.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:namer_app/utils/constants.dart';
+import 'package:provider/provider.dart';
+import 'dart:ui';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Design 1',
+      theme: ThemeData(primaryColor: COLOR_WHITE, textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT, fontFamily: "Montserrat", colorScheme: ColorScheme.fromSwatch().copyWith(secondary: COLOR_DARK_BLUE)),
+      home: LandingPage(),
+    );
+  }
+}
